f6aa1a255f9d69494befba841a09a92f14208457
i-am-neegr <martial.artem@gmail.com>
2026-04-24 02:16:30 +0300

    handlers imports

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import *
 from .keyboards import main_menu
 from .models import Category, HeroSuggestion, PremiereSubscriber, PartnerRequest
 
